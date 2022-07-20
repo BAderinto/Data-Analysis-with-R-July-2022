30747f3d5f2ab1db4bfbb8e0ea71ce388a93d25f
BDataLab <businessdatalab@gmail.com>
2022-07-20 19:41:12 +0100

    ok

--- R Basics/script_r_basics.R
+++ R Basics/script_r_basics.R
@@ -21,9 +21,6 @@ x <- "c"   ## character
 x <- 29    ## integer
 x <- 2+4i  ## complex
 
-
-# Data structures in R (Vector, List, Matrice, Array, Data frame)
-
 # Vector - same type, mixed data types are not allowed
 
 x <- c(0.5, 0.6, 1)       ## numeric
@@ -38,20 +35,12 @@ x <- vector("numeric", length = 10)
 x
 
 # Mixing Objects
-# different classes of R objects get mixed together. 
-# So what happens with the following code?
-
+# implicit coercion
 y <- c(1.7, "a")   ## character
 y <- c(TRUE, 2)    ## numeric
 y <- c("a", TRUE)  ## character
 
-# When different objects are mixed in a vector, coercion occurs so that every element in the vector is of the same class.
-
-# effect of implicit coercion - 
-# R tries to find a way to represent all of the objects in the vector in a reasonable fashion. 
-
-# Explicit Coercion - from one class to another using the as.* functions, if available.
-
+# explicit coercion
 x <- 0:6
 class(x)
 
@@ -70,10 +59,17 @@ as.logical(x)
 
 as.complex(x)
 
+# Lists 
+
+x <- list(1, "a", TRUE, 1 + 4i) 
+x
+
+
+x <- vector("list", length = 5)
+x
+
 
 # Matrices
-# Matrices are vectors with a dimension attribute. 
-# The dimension attribute is itself an integer vector of length 2 (number of rows, number of columns)
 
 my_mat <- matrix(nrow = 2, ncol = 3) 
 my_mat
@@ -82,10 +78,6 @@ my_mat
 dim(my_mat)
 
 attributes(my_mat)
-
-
-# Matrices are constructed column-wise, 
-# starting from the “upper left” corner and running down the columns.
 
 m <- matrix(1:6, nrow = 2, ncol = 3) 
 m
@@ -106,20 +98,6 @@ y <- 10:12
 cbind(x, y)
 
 rbind(x, y) 
-
-# Lists 
-# - special type of vector that can contain elements of different classes. 
-# - very important data type in R Lists, in combination with the various “apply” functions discussed later, make for a powerful combination.
-
-#Lists can be explicitly created using the list() function, which takes an arbitrary number of arguments.
-
-x <- list(1, "a", TRUE, 1 + 4i) 
-x
-
-# We can also create an empty list of a prespecified length with the vector() function
-
-x <- vector("list", length = 5)
-x
 
 
 
